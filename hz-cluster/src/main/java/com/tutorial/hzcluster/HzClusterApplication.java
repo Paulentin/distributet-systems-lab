7dcba8eb0d5748bd80c331aff95bdc4f995c8ca9
pavlozabara-nc <zabarapasha09@gmail.com>
2025-04-27 15:35:20 +0300

    add missing hz proj files

---
+++ hz-cluster/src/main/java/com/tutorial/hzcluster/HzClusterApplication.java
@@ -0,0 +1,14 @@
+package com.tutorial.hzcluster;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzClusterApplication.class, args);
+    }
+
+}
